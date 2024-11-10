d89d68ae10a5aee0d3917bde17dfb5e59fa211d9
seyfu-t <seyfullahtalay@hotmail.com>
2024-11-10 16:26:24 +0100

    final form without pcap

--- programm_b.py
+++ programm_b.py
@@ -27,11 +27,11 @@ def handle_packet(packet):
 
                 if data.startswith("START:"):
                     total_packets = int(data.split(":")[1])
-                    print(f"START message received. Expecting {total_packets} packets.")
+                    print(f"START message received. Expecting {total_packets} packets.\n\n\n")
                     return
 
                 if data == "STOP":
-                    print("STOP message received.")
+                    print(f"\n\n\nSTOP message received.")
                     stop_received = True  # Set flag when STOP is received
                     handle_missing_packets_queue()
                     return
@@ -41,14 +41,15 @@ def handle_packet(packet):
                 chunk = data[4:-32]
                 received_checksum = data[-32:]
 
-                # Simulate checksum error for packet 6
-                if skip and packet_num == 6:
-                    received_checksum = received_checksum[:2] + 'b' + received_checksum[3:]
-                    skip = False
+                # # Simulate checksum error for packet 6
+                # if skip and packet_num == 6:
+                #     received_checksum = received_checksum[:2] + 'b' + received_checksum[3:]
+                #     skip = False
 
                 # Verify checksum and store valid packets
                 if calculate_checksum(chunk) == received_checksum:
-                    print(f"Packet {packet_num} integrity confirmed.")
+                    decoded_chunk = base64.b64decode(chunk).decode('utf-8')
+                    print(f"{decoded_chunk}", end='')
                     received_packets[packet_num] = chunk
                 else:
                     print(f"Checksum error in packet {packet_num}. Adding to queue for retransmission.")
@@ -66,7 +67,6 @@ def handle_missing_packets_queue():
         save_data_to_file()
         stop_received = False  # Reset the flag after saving data
         return
-
     # Request retransmission for packets in the queue
     for packet_num in missing_packets_queue:
         if packet_num not in received_packets:
